15899f9611869670e4d8cd1078d08097e9a61dc7
everulicess <ulices.vaca12@gmail.com>
2024-05-02 11:19:33 +0200

    functionility for the menu

---
+++ Assets/Scripts/Menus/StartMenu/StartMenuManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public enum MyScenes
+{
+    StrtingMenu,
+    Game
+}
+
+public class StartMenuManager : MonoBehaviour
+{
+    [SerializeField] GameObject CreditsPanel;
+    [SerializeField] GameObject OptionsPanel;
+
+    [SerializeField] MyScenes sceneToLoad;
+
+    private void Start()
+    {
+        HideAllPanels();
+    }
+    public void HideAllPanels()
+    {
+        CreditsPanel.SetActive(false);
+        OptionsPanel.SetActive(false);
+    }
+
+    //CREDITS
+    public void CreditsPanelToggle()
+    {
+        CreditsPanel.SetActive(!CreditsPanel.activeInHierarchy);
+    }
+
+    //OPTIONS
+    public void OptionsPanelToggle()
+    {
+        OptionsPanel.SetActive(!OptionsPanel.activeInHierarchy);
+    }
+
+    public void OnVolumeChanged()
+    {
+
+    }
+
+    //PLAY
+    public void OnPlayClicked()
+    {
+        SceneManager.LoadSceneAsync(sceneToLoad.ToString());
+    }
+
+    //QUIT
+    public void OnQuitClicked()
+    {
+        Application.Quit();
+    }
+}
